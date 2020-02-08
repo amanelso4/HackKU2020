b0150c2ef7eb4ac32c1a57914c72cc546fcd9c88
Josh Haxton <haxtonjr@gmail.com>
2020-02-08 16:39:57 -0600

    date stuff

--- src/assets/cities.ts
+++ src/assets/cities.ts
@@ -5,7 +5,6 @@ export class City {
   Population: number;
   ImageLink: string;
   Link: string;
-  /*
   Jan: number;
   Feb: number;
   March: number;
@@ -18,74 +17,158 @@ export class City {
   Oct: number;
   Nov: number;
   Dec: number;
-  */
+
   Attraction: string;
 }
 
 export const citylist: City[] = 
 [
-   { 
-     Name: "New York",
+  { 
+    Name: "New York",
     State: "NY",
     COL: 187,
     Population: 8770000,
     ImageLink: "https://specials-images.forbesimg.com/imageserve/5d93909224fbf10007b8a1fd/960x0.jpg?fit=scale",
     Link: "https://www1.nyc.gov/",
-    Attraction: "Statue of Liberty, Central Park, Times Square, Empire State Building, Rockefeller Center, Grand Central Station"
-    },
-    { 
-      Name: "Lawrence",
-     State: "KS",
-     COL: 93,
-     Population: 97286,
-     ImageLink: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fadmissions.ku.edu%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fcaptioned_media%2Fpublic%2F2017-03%2FRS12514_103274_ExtdCanvas%2520%25281%2529_0.jpg%3Fitok%3D6Q0Eiu9m&imgrefurl=https%3A%2F%2Fadmissions.ku.edu%2Fvisiting-campus%2Flawrence&tbnid=MlUHKX0tzu6HvM&vet=12ahUKEwj8qNqF8cLnAhUExKwKHcetDDEQMygCegUIARCDAg..i&docid=riG5Lhbg6qDqTM&w=2334&h=1312&q=lawrence%20ks&ved=2ahUKEwj8qNqF8cLnAhUExKwKHcetDDEQMygCegUIARCDAg",
-     Link: "lawrenceks.org",
-     Attraction: "Clinton State Park, Spencer Museum of Art, Massachussets Street, KU Natural History Museum, University of Kansas, Haskell Indian Nations University, Watkins Museum of History, Castle Tea Room"
-     },
-     { 
-      Name: "Los Angeles",
-     State: "CA",
-     COL: 148,
-     Population: 12447000,
-     ImageLink: "https://www.lacity.org/",
-     Link: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/20190616154621%21Echo_Park_Lake_with_Downtown_Los_Angeles_Skyline.jpg/1200px-20190616154621%21Echo_Park_Lake_with_Downtown_Los_Angeles_Skyline.jpg",
-     Attraction: "Universal Studios Hollywood, Disneyland Park, Griffith Observatory, Hollywood Sign, Hollywood Walk-of-Fame, Hollywood Boulevard"
-     },
-     { 
-      Name: "St Louis",
-     State: "MO",
-     COL: 81,
-     Population: 302838,
-     ImageLink: "https://www.wheretraveler.com/sites/default/files/styles/promoted_image/public/helicopter10.29.jpg?itok=Hpczk6GE&timestamp=1484692151",
-     Link: "https://www.stlouis-mo.gov/",
-     Attraction: "Gateway Arch, Missouri Botantical Garden, City Museum, Saint Louis Zoo, Busch Stadium"
-     },
-     { 
-      Name: "Chicago",
-     State: "IL",
-     COL: 107,
-     Population: 2705994,
-     ImageLink: "https://s3-prod.chicagobusiness.com/GettyImages-1057157166.jpg",
-     Link: "https://www.chicago.gov/city/en.html",
-     Attraction: "Millennium Park, Willis Tower, Navy Pier, Skydeck Chicago, Shedd Aquarium"
-     },
-     { 
-      Name: "Houston",
-     State: "TX",
-     COL: 97,
-     Population: 2162000,
-     ImageLink: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Houston_from_Sabine_Park.jpg/600px-Houston_from_Sabine_Park.jpg",
-     Link: "https://www.houstontx.gov/",
-     Attraction: "Space Center Houston, Houston Museum of Natural Science, Houston Zoo, Johnson Space Center"
-     },
-     { 
-      Name: "",
-     State: "",
-     COL: 1,
-     Population: 1,
-     ImageLink: "",
-     Link: "",
-     Attraction: ""
-     },
+    Attraction: "Statue of Liberty, Central Park, Times Square, Empire State Building, Rockefeller Center, Grand Central Station",
+    Jan: 0,
+    Feb: 0,
+    March: 0,
+    April: 0,
+    May: 0,
+    June: 0,
+    July: 0,
+    Aug: 0,
+    Sept: 0,
+    Oct: 0,
+    Nov: 0,
+    Dec: 0
+  },
+  { 
+    Name: "Lawrence",
+    State: "KS",
+    COL: 93,
+    Population: 97286,
+    ImageLink: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fadmissions.ku.edu%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fcaptioned_media%2Fpublic%2F2017-03%2FRS12514_103274_ExtdCanvas%2520%25281%2529_0.jpg%3Fitok%3D6Q0Eiu9m&imgrefurl=https%3A%2F%2Fadmissions.ku.edu%2Fvisiting-campus%2Flawrence&tbnid=MlUHKX0tzu6HvM&vet=12ahUKEwj8qNqF8cLnAhUExKwKHcetDDEQMygCegUIARCDAg..i&docid=riG5Lhbg6qDqTM&w=2334&h=1312&q=lawrence%20ks&ved=2ahUKEwj8qNqF8cLnAhUExKwKHcetDDEQMygCegUIARCDAg",
+    Link: "lawrenceks.org",
+    Attraction: "Clinton State Park, Spencer Museum of Art, Massachussets Street, KU Natural History Museum, University of Kansas, Haskell Indian Nations University, Watkins Museum of History, Castle Tea Room",
+    Jan: 0,
+    Feb: 0,
+    March: 0,
+    April: 0,
+    May: 0,
+    June: 0,
+    July: 0,
+    Aug: 0,
+    Sept: 0,
+    Oct: 0,
+    Nov: 0,
+    Dec: 0
+  },
+  { 
+    Name: "Los Angeles",
+    State: "CA",
+    COL: 148,
+    Population: 12447000,
+    ImageLink: "https://www.lacity.org/",
+    Link: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/20190616154621%21Echo_Park_Lake_with_Downtown_Los_Angeles_Skyline.jpg/1200px-20190616154621%21Echo_Park_Lake_with_Downtown_Los_Angeles_Skyline.jpg",
+    Attraction: "Universal Studios Hollywood, Disneyland Park, Griffith Observatory, Hollywood Sign, Hollywood Walk-of-Fame, Hollywood Boulevard",
+    Jan: 0,
+    Feb: 0,
+    March: 0,
+    April: 0,
+    May: 0,
+    June: 0,
+    July: 0,
+    Aug: 0,
+    Sept: 0,
+    Oct: 0,
+    Nov: 0,
+    Dec: 0
+  },
+  { 
+    Name: "St Louis",
+    State: "MO",
+    COL: 81,
+    Population: 302838,
+    ImageLink: "https://www.wheretraveler.com/sites/default/files/styles/promoted_image/public/helicopter10.29.jpg?itok=Hpczk6GE&timestamp=1484692151",
+    Link: "https://www.stlouis-mo.gov/",
+    Attraction: "Gateway Arch, Missouri Botantical Garden, City Museum, Saint Louis Zoo, Busch Stadium",
+    Jan: 0,
+    Feb: 0,
+    March: 0,
+    April: 0,
+    May: 0,
+    June: 0,
+    July: 0,
+    Aug: 0,
+    Sept: 0,
+    Oct: 0,
+    Nov: 0,
+    Dec: 0
+  },
+  { 
+    Name: "Chicago",
+    State: "IL",
+    COL: 107,
+    Population: 2705994,
+    ImageLink: "https://s3-prod.chicagobusiness.com/GettyImages-1057157166.jpg",
+    Link: "https://www.chicago.gov/city/en.html",
+    Attraction: "Millennium Park, Willis Tower, Navy Pier, Skydeck Chicago, Shedd Aquarium",
+    Jan: 0,
+    Feb: 0,
+    March: 0,
+    April: 0,
+    May: 0,
+    June: 0,
+    July: 0,
+    Aug: 0,
+    Sept: 0,
+    Oct: 0,
+    Nov: 0,
+    Dec: 0
+  },
+  { 
+    Name: "Houston",
+    State: "TX",
+    COL: 97,
+    Population: 2162000,
+    ImageLink: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Houston_from_Sabine_Park.jpg/600px-Houston_from_Sabine_Park.jpg",
+    Link: "https://www.houstontx.gov/",
+    Attraction: "Space Center Houston, Houston Museum of Natural Science, Houston Zoo, Johnson Space Center",
+    Jan: 0,
+    Feb: 0,
+    March: 0,
+    April: 0,
+    May: 0,
+    June: 0,
+    July: 0,
+    Aug: 0,
+    Sept: 0,
+    Oct: 0,
+    Nov: 0,
+    Dec: 0 
+  },
+  { 
+    Name: "",
+    State: "",
+    COL: 0,
+    Population: 0,
+    ImageLink: "",
+    Link: "",
+    Attraction: "",
+    Jan: 0,
+    Feb: 0,
+    March: 0,
+    April: 0,
+    May: 0,
+    June: 0,
+    July: 0,
+    Aug: 0,
+    Sept: 0,
+    Oct: 0,
+    Nov: 0,
+    Dec: 0
+  },
 
 ];
