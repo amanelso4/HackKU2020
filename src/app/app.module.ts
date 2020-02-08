64a8fcca8ba77f21b1ebc51bd7710c6ff3ede009
Amanda <amandanelson2018@gmail.com>
2020-02-08 09:48:06 -0600

    Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CityDetailsComponent } from './city-details/city-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CityDetailsComponent
   ],
   imports: [
     BrowserModule,
