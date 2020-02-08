c8566c2ab0e545ffc66ec3a29266446bdd79cdc2
Amanda <amandanelson2018@gmail.com>
2020-02-08 10:20:14 -0600

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CityDetailsComponent } from './city-details/city-details.component';
+
+const appRoutes: Routes = [
+  {path: 'home', component: HomeComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {path: '', redirectTo: '/', pathMatch: 'full'},
+];
 
 @NgModule({
   declarations: [
@@ -14,7 +21,11 @@ import { CityDetailsComponent } from './city-details/city-details.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { useHash: true }
+    ),
   ],
   providers: [],
   bootstrap: [AppComponent]
