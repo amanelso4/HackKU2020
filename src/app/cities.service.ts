f2e5e826a87fae95430dba1b72f091fe4661277f
Amanda <amandanelson2018@gmail.com>
2020-02-08 12:11:11 -0600

    Working on http client

--- src/app/cities.service.ts
+++ src/app/cities.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+export class City {
+  Name: string;
+  State: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class CitiesService {
 
-  constructor() { }
+  constructor(    
+    private http: HttpClient
+  ) { }
+
+  getSingleCity(Name: string): Observable<City> {
+    return this.http.get<Form>(`${this.formUrl}/${formNumber}`)
+      .pipe(catchError(this.handleError));
+  }
+
 }
--- src/app/city-details/city-details.component.ts
+++ src/app/city-details/city-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CityList } from './../assets/cities.ts';
 
 @Component({
   selector: 'app-city-details',
